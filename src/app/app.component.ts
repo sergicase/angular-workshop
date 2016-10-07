23d8ba9522594f7ffa6b7f36c3e2895514347e07
Sergi Case Massana <sergi.casemassana@interone.de>
2016-10-07 17:09:35 +0200

    Binding data to module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,16 @@
 import IComponentOptions = angular.IComponentOptions
-
+import {ICar} from "./cars/car.interface";
+import {CarsService} from "./cars/cars.service";
 //4/ App component is just a wrapper for products component now
 export const AppComponent: IComponentOptions = {
-  controller: class {},
-  template: `<cars></cars>`,
+  controller: class {
+    public cars: ICar[];
+
+    constructor(Cars: CarsService) {
+      'ngInject';
+
+      this.cars = Cars.cars;
+    }
+  },
+  template: `<cars cars="$ctrl.cars"></cars>`,
 }
--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -5,6 +5,11 @@ import IComponentOption = angular.IComponentOptions;
 
 /// And using that interface helps us to properly configure the component
 export const CarsComponent: IComponentOption = {
+  
+   //3/ We can receive data in component by declaring bindings
+  bindings: {
+    products: '<'
+  },
   //10/ Now controller and template are next to each other
   controller: class {
     public cars: ICar[]

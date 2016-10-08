646802382ef3813aed47774a2266d5c47bc21375
Oleg Korol <oleg@olegkorol.de>
2016-10-08 12:34:31 +0200

    add factory function

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,14 @@ import * as angular from 'angular'
 import IModule = angular.IModule
 import { CarsModule } from './cars/cars'
 import { AppComponent } from './app.component'
+import {CarsFactory} from "./cars/cars.factory";
+import {CarsService} from "./cars/cars.service";
 
 //5/ Let's create a module for all components
 export const ComponentsModule: IModule = angular
-  .module('app.components', [
-    CarsModule.name,
-  ])
-  .component('app', AppComponent)
+    .module('app.components', [
+        CarsModule.name,
+    ])
+    .component('app', AppComponent)
+    .service('CarsService', CarsService)
+    .factory('cars', CarsFactory);
--- src/app/cars/cars.service.ts
+++ src/app/cars/cars.service.ts
@@ -7,12 +7,12 @@ export class CarsService {
         { name: 'Huracan', speed: 320, brand: 'Lamborghini' },
         { name: 'Veyron', speed: 400, brand: 'Buggati' },
         { name: 'Astra', speed: 400, brand: 'Opel' }
-    ]
+    ];
 
      public orderByName (cars: ICar[]) {
        return cars.sort(function (a, b) {
-            let a_aux:string  = a.name.toLowerCase()
-            let b_aux:string = b.name.toLowerCase()
+            let a_aux:string  = a.name.toLowerCase();
+            let b_aux:string = b.name.toLowerCase();
             if (a_aux > b_aux) {
                 return 1;
             }
@@ -26,8 +26,8 @@ export class CarsService {
 
     public orderByBrand (cars: ICar[]) {
        return cars.sort(function (a, b) {
-            let a_aux:string  = a.brand.toLowerCase()
-            let b_aux:string = b.brand.toLowerCase()
+            let a_aux:string  = a.brand.toLowerCase();
+            let b_aux:string = b.brand.toLowerCase();
             if (a_aux > b_aux) {
                 return 1;
             }

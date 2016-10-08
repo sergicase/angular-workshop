f8c5de529b08acf76dda74df7e123a04bfd01f72
Sergi Case Massana <sergi.casemassana@interone.de>
2016-10-08 10:41:37 +0200

    Add sort logic service/component

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -5,7 +5,7 @@ const { listElement } = require('./cars.scss') // tslint:disable-line
 
 import { ICar } from './car.interface'
 import IComponentOption = angular.IComponentOptions;
-
+import { CarsService } from './cars.service'
 /// And using that interface helps us to properly configure the component
 export const CarsComponent: IComponentOption = {
   
@@ -16,17 +16,44 @@ export const CarsComponent: IComponentOption = {
   //10/ Now controller and template are next to each other
   controller: class {
     public cars: ICar[]
+    public order_by: string
+    public car_service: CarsService
 
-    constructor(Cars: any) {
+    constructor(Cars: CarsService) {
       'ngInject';
 
-      this.cars = Cars.cars
+      this.car_service = Cars
+      this.cars = this.car_service.cars
     }
+
+    sortList(){
+       switch (this.order_by){
+         case "brand":
+          this.cars = this.car_service.orderByBrand(this.cars);
+         break;
+         case "name":
+          this.cars = this.car_service.orderByName(this.cars);
+         break;
+         default:
+           this.cars = this.car_service.orderByName(this.cars);
+          break;
+
+       }
+     
+    }
+    
+    
+
   },
   template: `
     <div>
       <h1>Shopping list</h1>
       <ul>
+        <input type="button" value="Sort By" ng-click="$ctrl.sortList()"/>
+        <select ng-model="$ctrl.order_by">
+          <option value="brand">Brand</option>
+          <option value="name">Name</option>
+        </select>
         <!-- Now we're using $ctrl instead of vm -->
         <li ng-repeat="car in $ctrl.cars">
           {{ car.name }} - <span>{{ car.speed }}</span> - {{car.brand}}
--- src/app/cars/cars.service.ts
+++ src/app/cars/cars.service.ts
@@ -6,5 +6,40 @@ export class CarsService {
         { name: 'R8', speed: 270, brand: 'Audi' },
         { name: 'Huracan', speed: 320, brand: 'Lamborghini' },
         { name: 'Veyron', speed: 400, brand: 'Buggati' },
+        { name: 'Astra', speed: 400, brand: 'Opel' }
     ]
+
+     public orderByName (cars: ICar[]) {
+       return cars.sort(function (a, b) {
+            let a_aux:string  = a.name.toLowerCase()
+            let b_aux:string = b.name.toLowerCase()
+            if (a_aux > b_aux) {
+                return 1;
+            }
+            if (a_aux < b_aux) {
+                return -1;
+            }
+            // a must be equal to b
+            return 0;
+        });
+    }; 
+
+    public orderByBrand (cars: ICar[]) {
+       return cars.sort(function (a, b) {
+            let a_aux:string  = a.brand.toLowerCase()
+            let b_aux:string = b.brand.toLowerCase()
+            if (a_aux > b_aux) {
+                return 1;
+            }
+            if (a_aux < b_aux) {
+                return -1;
+            }
+            // a must be equal to b
+            return 0;
+        });
+    }; 
+
+    public getCars (){
+        return  this.cars;
+    }
 }
